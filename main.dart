f179195aa3b3f4ff1c89de7e0f74466659334b91
MiguelAuc11 <miguecalde11@gmail.com>
2022-10-06 23:52:18 -0500

    commit del lib main.dart

--- main.dart
+++ main.dart
@@ -1,28 +1,72 @@
+import 'dart:async';
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'dart:developer' as devtools show log;
+import 'dart:convert';
 
-void main() {
-  runApp(const MyApp());
+extension Log on Object {
+  void log() => devtools.log(toString());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
+void main() { runApp(MaterialApp(
+      title: 'Material App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: null,
+      debugShowCheckedModeBanner: false,
+        home: const HomePage(),
+    ),
+  );
+}
+
+@immutable
+class Person{
+  final String name;
+  final int age;
+
+  const Person({
+    required this.name,
+    required this.age,
+  });
+
+  Person.fromJson(Map<String, dynamic> json)
+    : name = json['name'] as String,
+      age = json['age'] as int;
+
+  @override 
+  String toString() {
+    return 'Person ($name, $age, years old)';
+  }
+
+}
+
+const url = 'http://127.0.0.1:5500/apis/people.json';
+
+Future<Iterable<Person>> parseJson() => HttpClient()
+  .getUrl(Uri.parse(url))
+  .then((req) => req.close())
+  .then((resp) => resp.transform(utf8.decoder).join())
+  .then((str) => json.decode(str) as List<dynamic>)
+  .then((json) => json.map((e) => Person.fromJson(e)));
+
+void testIt() {
+  final persons = parseJson();
+  persons.log();
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    testIt();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Material App Bar'),
+      ),
+      body: const Center(
+        child: Text('Hello World'),
+      ),
     );
   }
 }
-
-
-/// doc actualizacion git
-/// nueva linea con ejemplo git status
-/// linea 3 de la modificacion
-/// modificacion 4 
-/// /// quinto cambio en linea de git
